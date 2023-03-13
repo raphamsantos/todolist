8f35d5669e0ff7465de5e98d263f8516d8f14c24
raphamsantos <raphamndds@gmail.com>
2023-03-13 18:05:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Todo List VnW - Feito em grupo
+
+Rodando o projeto:
+
+```bash
+$ git clone https://github.com/CauaRodrigues/todolist-vnw.git todo-list
+$ cd todo-list
+$ npm install
+$ npm start
+```
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GlobalStyles } from "./globalStyles";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<GlobalStyles />
+
+		<App />
+	</React.StrictMode>
+);
